e1b8b34d6483046abe51380ee8ee6caa4c6b42c1
venzy <david.venz@gmail.com>
2025-03-27 23:38:52 +1000

    Ch1 Servers L5 Fileservers

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
 }
