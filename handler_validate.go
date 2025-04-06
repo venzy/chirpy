51d780dd7556ba94c68fb2aa9577c9d59ad2e4f7
venzy <david.venz@gmail.com>
2025-04-06 22:22:12 +1000

    Ch4 JSON L6 The Profane

--- handler_validate.go
+++ handler_validate.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func (cfg *apiConfig) handleValidate(response http.ResponseWriter, request *http.Request) {
@@ -12,12 +13,8 @@ func (cfg *apiConfig) handleValidate(response http.ResponseWriter, request *http
 		Body string `json:"body"`
 	}
 
-	type errorResponse struct {
-		Error string `json:"error"`
-	}
-
 	type successResponse struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(request.Body)
@@ -30,26 +27,56 @@ func (cfg *apiConfig) handleValidate(response http.ResponseWriter, request *http
 	}
 
 	if len(params.Body) > cfg.maxChirpLength {
-		responseBody, err := json.Marshal(errorResponse{
-			Error: fmt.Sprintf("chirp too long, must be less than or equal to %d chars", cfg.maxChirpLength),
-		})
-		if err != nil {
-			log.Printf("Error encoding error response: %s\n", err)
-			response.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		response.Header().Set("Content-Type", "application/json; charset=utf-8")
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write(responseBody)
+		respondWithError(response,
+			http.StatusBadRequest,
+			fmt.Sprintf("chirp too long, must be less than or equal to %d chars", cfg.maxChirpLength))
 	} else {
-		responseBody, err := json.Marshal(successResponse{Valid: true})
-		if err != nil {
-			log.Printf("Error encoding success response: %s\n", err)
-			response.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		response.Header().Set("Content-Type", "application/json; charset=utf-8")
-		response.WriteHeader(http.StatusOK)
-		response.Write(responseBody)
+		respondWithJSON(response,
+			http.StatusOK,
+			successResponse{CleanedBody: cleanBody(params.Body)})
 	}
+}
+
+func respondWithError(response http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	responseBody, err := json.Marshal(errorResponse{
+		Error: msg,
+	})
+	if err != nil {
+		log.Printf("Error encoding error response: %s\n", err)
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	response.Header().Set("Content-Type", "application/json; charset=utf-8")
+	response.WriteHeader(code)
+	response.Write(responseBody)
+}
+
+func respondWithJSON(response http.ResponseWriter, code int, payload interface{}) {
+	responseBody, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error encoding success response: %s\n", err)
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	response.Header().Set("Content-Type", "application/json; charset=utf-8")
+	response.WriteHeader(code)
+	response.Write(responseBody)
+}
+
+func cleanBody(body string) string {
+	splitBody := strings.Split(body, " ")
+	newSplitBody := []string{}
+	for _, word := range splitBody {
+		lower := strings.ToLower(word)
+		if lower == "kerfuffle" || lower == "sharbert" || lower == "fornax" {
+			newSplitBody = append(newSplitBody, "****")
+		} else {
+			newSplitBody = append(newSplitBody, word)
+		}
+	}
+	return strings.Join(newSplitBody, " ")
 }
