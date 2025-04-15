83faf7429a6dff406138c3a46d72a1c0565d98ac
venzy <david.venz@gmail.com>
2025-04-15 22:52:20 +1000

    Ch6 Authentication L6 JWTs

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 // Note - if you're concerned about how strings and temporary byte slices hang
 // around in memory until garbage collected - IMO there seems to be little
@@ -16,4 +23,41 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(hash, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	now := time.Now().UTC()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(now.Add(expiresIn)),
+		Subject: userID.String(),
+	})
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	// This API is a little weird, and the documentation is pretty awful.
+	// It looks like you have to pass in a stack-local 'claims' to parse into ...
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	// ... but then you can still access it via the returned token
+	id, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	if id == "" {
+		return uuid.UUID{}, fmt.Errorf("subject claim is missing")
+	}
+
+	return uuid.Parse(id)
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestHash(t *testing.T) {
@@ -32,5 +36,51 @@ func TestCheckHashFail(t * testing.T) {
 	err := CheckPasswordHash("not_!@##$@_a_$%R@#$%_real_@#$@#$!_hash", password)
 	if err == nil {
 		t.Errorf("CheckPasswordHash() should have failed for a bogus hash")
+	}
+}
+
+func TestValidJWT(t * testing.T) {
+	// Create JWT
+	userID := uuid.New()
+	secret := "eNc0d4_1f3"
+	jwt, err := MakeJWT(userID, secret, 5 * time.Second)
+	if err != nil {
+		t.Errorf("MakeJWT() should have succeeded, err was: %s", err)
+	}
+	t.Logf("JWT: %s", jwt)
+
+	// Validate JWT
+	validatedUserID, err := ValidateJWT(jwt, secret)
+	if err != nil {
+		t.Errorf("ValidateJWT() should have succeeded, err was: %s", err)
+	}
+	t.Logf("UserID: %s", validatedUserID)
+
+	if userID != validatedUserID {
+		t.Errorf(`validated user ID should match original user ID:
+	Original:  %s
+	Validated: %s`,
+			userID, validatedUserID)
+	}
+}
+
+func TestExpiredJWT(t * testing.T) {
+	// Create JWT
+	userID := uuid.New()
+	secret := "eNc0d4_1f3"
+	jwt, err := MakeJWT(userID, secret, 0 * time.Second)
+	if err != nil {
+		t.Errorf("MakeJWT() should have succeeded, err was: %s", err)
+	}
+	t.Logf("JWT: %s", jwt)
+
+	// Validate JWT
+	_, err = ValidateJWT(jwt, secret)
+	if err == nil {
+		t.Errorf("ValidateJWT() should have failed")
+	}
+	
+	if !strings.Contains(err.Error(), "xpire") {
+		t.Errorf("ValidateJWT() should have failed with expiry message, but err was %s", err)
 	}
 }
